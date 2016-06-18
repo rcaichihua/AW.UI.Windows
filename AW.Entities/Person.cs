6af77d8bc48c2d0cc7b9e35aef0236f9b0a937e1
VS2015_VS <rcaichihua@gmail.com>
2016-06-18 12:39:55 -0500

    BusinessEntityID

--- AW.Entities/Person.cs
+++ AW.Entities/Person.cs
@@ -11,7 +11,7 @@ namespace AW.Entities
         public int BusinessEntityID { get; set; }
         public string PersonType { get; set; }
         public bool NameStyle { get; set; }
-        public int Title { get; set; }
+        public string Title { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
