c65ecb90c49c1fe4fe3428cd17170b88ef730c58
VS2015_VS <rcaichihua@gmail.com>
2016-06-17 19:43:30 -0500

    prueba

---
+++ AW.Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AW.Entities
+{
+    public class Employee
+    {
+        public int BusinessEntityID { get; set; }
+        public string NationalIDNumber { get; set; }
+        public string LoginId { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Int16 VacationHours { get; set; }
+
+    }
+}
