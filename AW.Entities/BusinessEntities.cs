ac83bae751eec3f36615271299060f6fbfe5d48e
VS2015_VS <rcaichihua@gmail.com>
2016-06-18 12:59:08 -0500

    Ejemplo con CodeFirst

--- AW.Entities/BusinessEntities.cs
+++ AW.Entities/BusinessEntities.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AW.Entities
 {
     public class BusinessEntities
     {
+        [Key]
         public int BusinessEntityID { get; set; }
         public Guid rowguid { get; set; }
         public DateTime ModifiedDate { get; set; }
